7373f8cba5c26cfa7dc44964083f0d8f291a827b
b1narylife <damiensedgwick@icloud.com>
2021-01-08 14:51:06 +0000

    Stripped away scaffolding

---
+++ src/pages/index.js
@@ -0,0 +1,16 @@
+import Head from 'next/head';
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Job Application Tracker</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main>
+        <h1>Job Application Tracker</h1>
+      </main>
+    </div>
+  );
+}
