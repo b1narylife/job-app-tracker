18179f3600a636eef47adacb136f9d2a2a6be725
b1narylife <damiensedgwick@icloud.com>
2021-01-08 15:29:56 +0000

    changed body colour to dark

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,8 +8,8 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main>
-        <header className="header py-3 bg-gray-800 text-white">
+      <main className="bg-gray-800 h-screen">
+        <header className="header py-3 text-white shadow-2xl">
           <div className="container flex flex-row items-center justify-between mx-auto">
             <div className="branding flex flex-row items-center">
               <img className="mx-2" src="" alt="Logo" />
@@ -19,6 +19,7 @@ export default function Home() {
             <div className="flex flex-row items-center">
               <div className="user mx-2">User</div>
               <div className="settings mx-2">Cog</div>
+              <div className="dark-mode mx-2">Dark</div>
             </div>
           </div>
         </header>
