2b27b95e7cd331ca917c09e9d259a8298d908445
b1narylife <damiensedgwick@icloud.com>
2021-01-08 15:44:57 +0000

    Added basic dark mode

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,9 @@
 import Head from 'next/head';
+import { useState } from 'react';
 
-export default function Home() {
+const Home = () => {
+  const [isDarkMode, setIsDarkMode] = useState(true);
+
   return (
     <div>
       <Head>
@@ -8,8 +11,16 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className="bg-gray-800 h-screen">
-        <header className="header py-3 text-white shadow-2xl">
+      <main
+        className={isDarkMode ? 'bg-gray-800 h-screen' : 'bg-gray-200 h-screen'}
+      >
+        <header
+          className={
+            isDarkMode
+              ? 'text-gray-200 header py-3 shadow-2xl'
+              : 'text-gray-800 header py-3 shadow-2xl'
+          }
+        >
           <div className="container flex flex-row items-center justify-between mx-auto">
             <div className="branding flex flex-row items-center">
               <img className="mx-2" src="" alt="Logo" />
@@ -19,11 +30,19 @@ export default function Home() {
             <div className="flex flex-row items-center">
               <div className="user mx-2">User</div>
               <div className="settings mx-2">Cog</div>
-              <div className="dark-mode mx-2">Dark</div>
+              <div className="dark-mode mx-2">
+                {isDarkMode ? (
+                  <button onClick={() => setIsDarkMode(!isDarkMode)}>L</button>
+                ) : (
+                  <button onClick={() => setIsDarkMode(!isDarkMode)}>D</button>
+                )}
+              </div>
             </div>
           </div>
         </header>
       </main>
     </div>
   );
-}
+};
+
+export default Home;
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,7 @@ module.exports = {
     './src/components/**/*.js',
     './src/components/**/*.tsx',
   ],
-  darkMode: false, // or 'media' or 'class'
+  darkMode: 'class',
   theme: {
     extend: {},
   },
