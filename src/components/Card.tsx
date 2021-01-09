ede305d5290574ac4133eef674d7b7c79f8b7c42
b1narylife <damiensedgwick@icloud.com>
2021-01-09 13:54:22 +0000

    card borders changing with dark mode fixed.

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -6,7 +6,7 @@ interface Card {
 
 export function Card({ title }: Card) {
   return (
-    <div className="card w-60 my-3 p-3 border-2">
+    <div className="card w-60 my-3 p-3 border-current border-2">
       <h1 className="capitalize text-center">{title}</h1>
     </div>
   );
