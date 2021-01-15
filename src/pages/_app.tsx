07019fc268e790d14d71bbaf25c705e3064ee1be
b1narylife <damiensedgwick@icloud.com>
2021-01-15 22:45:36 +0000

    revert last commit

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,23 +1,23 @@
 import type { AppProps /*, AppContext */ } from "next/app";
 import React, { useState, useEffect } from "react";
 import { DndProvider } from "react-dnd";
-import { HTML5Backend } from "react-dnd-html5-backend";
+// import { HTML5Backend } from "react-dnd-html5-backend";
 import { TouchBackend } from "react-dnd-touch-backend";
 
 import "../styles/global.css";
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
-  const [isTouch, setIsTouch] = useState(false);
-
-  useEffect(() => {
-    if (window.matchMedia("pointer: course")) {
-      setIsTouch(true);
-    }
-  }, []);
+  // const [isTouch, setIsTouch] = useState(false);
+  //
+  // useEffect(() => {
+  //   if (window.matchMedia("pointer: course")) {
+  //     setIsTouch(true);
+  //   }
+  // }, []);
 
   return (
     <DndProvider
-      backend={isTouch ? TouchBackend : HTML5Backend}
+      backend={TouchBackend}
       options={{
         enableMouseEvents: true,
       }}
