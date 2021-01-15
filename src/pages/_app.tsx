85631bab7595818dc3a6a93c16a099be5aca34d4
b1narylife <damiensedgwick@icloud.com>
2021-01-15 21:13:17 +0000

    WIP working on touch backend

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -7,32 +7,29 @@ import { HTML5Backend } from "react-dnd-html5-backend";
 import "../styles/global.css";
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
-  const [isMobile, setIsMobile] = useState(false);
-
-  const options = {
-    enableTouchEvents: true,
-    scrollAngleRanges: [{ start: 300 }, { end: 60 }, { start: 120, end: 240 }],
-  };
-
-  useEffect(() => {
-    if (
-      navigator.userAgent.match(/Android/i) ||
-      navigator.userAgent.match(/webOS/i) ||
-      navigator.userAgent.match(/iPhone/i) ||
-      navigator.userAgent.match(/iPad/i) ||
-      navigator.userAgent.match(/iPod/i) ||
-      navigator.userAgent.match(/BlackBerry/i) ||
-      navigator.userAgent.match(/Windows Phone/i)
-    ) {
-      setIsMobile(true);
-    }
-  }, [isMobile]);
+  // const [isMobile, setIsMobile] = useState(false);
+  //
+  // const options = {
+  //   enableTouchEvents: true,
+  //   scrollAngleRanges: [{ start: 300 }, { end: 60 }, { start: 120, end: 240 }],
+  // };
+  //
+  // useEffect(() => {
+  //   if (
+  //     navigator.userAgent.match(/Android/i) ||
+  //     navigator.userAgent.match(/webOS/i) ||
+  //     navigator.userAgent.match(/iPhone/i) ||
+  //     navigator.userAgent.match(/iPad/i) ||
+  //     navigator.userAgent.match(/iPod/i) ||
+  //     navigator.userAgent.match(/BlackBerry/i) ||
+  //     navigator.userAgent.match(/Windows Phone/i)
+  //   ) {
+  //     setIsMobile(true);
+  //   }
+  // }, [isMobile]);
 
   return (
-    <DndProvider
-      backend={isMobile ? TouchBackend : HTML5Backend}
-      options={options}
-    >
+    <DndProvider backend={TouchBackend} options={{ enableMouseEvents: true }}>
       <Component {...pageProps} />
     </DndProvider>
   );
