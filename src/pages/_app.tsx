7555729d04c91a344a5245db56f734be06f1432c
b1narylife <damiensedgwick@icloud.com>
2021-01-15 21:18:09 +0000

    added scroll options and deleted comments

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,5 @@
 import type { AppProps /*, AppContext */ } from "next/app";
-import React, { useEffect, useState } from "react";
+import React from "react";
 import { DndProvider } from "react-dnd";
 import { TouchBackend } from "react-dnd-touch-backend";
 import { HTML5Backend } from "react-dnd-html5-backend";
@@ -7,29 +7,18 @@ import { HTML5Backend } from "react-dnd-html5-backend";
 import "../styles/global.css";
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
-  // const [isMobile, setIsMobile] = useState(false);
-  //
-  // const options = {
-  //   enableTouchEvents: true,
-  //   scrollAngleRanges: [{ start: 300 }, { end: 60 }, { start: 120, end: 240 }],
-  // };
-  //
-  // useEffect(() => {
-  //   if (
-  //     navigator.userAgent.match(/Android/i) ||
-  //     navigator.userAgent.match(/webOS/i) ||
-  //     navigator.userAgent.match(/iPhone/i) ||
-  //     navigator.userAgent.match(/iPad/i) ||
-  //     navigator.userAgent.match(/iPod/i) ||
-  //     navigator.userAgent.match(/BlackBerry/i) ||
-  //     navigator.userAgent.match(/Windows Phone/i)
-  //   ) {
-  //     setIsMobile(true);
-  //   }
-  // }, [isMobile]);
-
   return (
-    <DndProvider backend={TouchBackend} options={{ enableMouseEvents: true }}>
+    <DndProvider
+      backend={TouchBackend}
+      options={{
+        enableMouseEvents: true,
+        scrollAngleRanges: [
+          { start: 300 },
+          { end: 60 },
+          { start: 120, end: 240 },
+        ],
+      }}
+    >
       <Component {...pageProps} />
     </DndProvider>
   );
