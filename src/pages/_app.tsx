e98cd6066ef9869f90a92601b331e3931a851ca1
b1narylife <damiensedgwick@icloud.com>
2021-01-15 22:14:58 +0000

    added logic for html or touch

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,14 +1,23 @@
 import type { AppProps /*, AppContext */ } from "next/app";
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { DndProvider } from "react-dnd";
+import { HTML5Backend } from "react-dnd-html5-backend";
 import { TouchBackend } from "react-dnd-touch-backend";
 
 import "../styles/global.css";
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
+  const [isTouch, setIsTouch] = useState(false);
+
+  useEffect(() => {
+    if (window.matchMedia("pointer: course")) {
+      setIsTouch(true);
+    }
+  }, []);
+
   return (
     <DndProvider
-      backend={TouchBackend}
+      backend={isTouch ? TouchBackend : HTML5Backend}
       options={{
         enableMouseEvents: true,
       }}
