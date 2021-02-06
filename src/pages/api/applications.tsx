3556e8f6fb92a75eb572ee06e9fbeed6c78f5c88
Damien Sedgwick <damiensedgwick@icloud.com>
2021-02-06 13:42:38 +0000

    Added more information to api data

--- src/pages/api/applications.tsx
+++ src/pages/api/applications.tsx
@@ -1,25 +1,37 @@
-import type { NextApiRequest, NextApiResponse } from 'next';
+import type { NextApiRequest, NextApiResponse } from "next";
 
 const applications = [
   {
     _id: 0,
-    companyName: 'Burger King',
-    status: 'Prospective',
+    companyName: "Burger King",
+    status: "Prospective",
+    notes: "",
+    interviewDate: null,
+    interviewTime: null,
   },
   {
     _id: 1,
-    companyName: 'KFC',
-    status: 'Applied',
+    companyName: "KFC",
+    status: "Applied",
+    notes: "My interview is with Kevin and Macey.",
+    interviewDate: "03/04/2021",
+    interviewTime: "14:00",
   },
   {
     _id: 2,
-    companyName: 'McDonalds',
-    status: 'Interviewing',
+    companyName: "McDonalds",
+    status: "Interviewing",
+    notes: "",
+    interviewDate: "02/02/2021",
+    interviewTime: "10:00",
   },
   {
     _id: 3,
-    companyName: 'Subway',
-    status: 'Outcome',
+    companyName: "Subway",
+    status: "Outcome",
+    notes: "",
+    interviewDate: "05/01/5021",
+    interviewTime: "15:30",
   },
 ];
 
